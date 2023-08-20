b21ee3b6ebf30ebb08a60d7c61ba41eaed98d75e
Matsuri <nhungmaitran1412@gmail.com>
2023-08-20 15:19:31 +0700

    Done landing page

--- js/home.js
+++ js/home.js
@@ -65,8 +65,7 @@ const clothingCat = {
 const catList = [electronicsCat, clothingCat];
 
 const renderProduct = (product) => {
-    return `<article class="col
-    ">
+    return `<article class="col d-inline-block">
         <div class="d-flex flex-column
         border border-2 h-100 rounded-2 overflow-hidden">
             <!-- Product image-->
@@ -74,7 +73,7 @@ const renderProduct = (product) => {
             <!-- Product details-->
             <div class="card-body p-4 text-start">
                 <!-- Product name-->
-                <h5 class="h5 fw-bolder line-clamp-3">${product.title}</h5>
+                <h6 class="h6 fw-bolder line-clamp-3">${product.title}</h6>
                 <!-- Product price-->
                 ${product.price}
             </div>
@@ -90,12 +89,20 @@ const renderProduct = (product) => {
 
 const renderContent = () => {
     return catList.map(category => {
-        return category.products.map(product => {
-            return renderProduct(product);
-        }
-        ).join('\n')
+        return `<section class="col">
+            <a role="button" class="fs-1 mb-4 mb-md-3 fw-bolder line-clamp-1 text-center text-dark 
+            link-underline link-dark link-underline-opacity-0 link-underline-opacity-75-hover">
+                ${category.category_name}
+            </a>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 gy-5 gx-4 gx-lg-10 overflow-x-scroll flex-nowrap scrollbar pb-5">
+            ${category.products.map((product) => {
+                return renderProduct(product);
+            }).join('\n')}
+            </div>
+        </section>`
     }).join('\n');
 }
 
 // 
-document.getElementById("home-content-product").appendChild(createElement('row row-cols-1 row-cols-sm-2 row-cols-md-3 gy-5 gx-4 gx-lg-10 ', renderContent()));
+document.getElementById("home-content-product").appendChild(createElement('row gy-5', renderContent()));
+// document.getElementById("home-content-product").appendChild(createElement('carousel-inner', renderContent()));
