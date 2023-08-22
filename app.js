a06314a989efafd6b8862eb1cfdcc8569a2bbd96
Giang Trong Duong <92968238+GiangTrongDuong@users.noreply.github.com>
2023-08-22 20:02:03 +0700

    Added connection to mongodb in app.js

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const uri = "mongodb+srv://eeet2099group2:eeet2099Pass@databaseapplicationproj.fexqmnq.mongodb.net/?retryWrites=true&w=majority";
+async function connect(){
+  try{
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+connect();
+
 require('dotenv').config();
 
 // view engine setup
@@ -16,6 +28,7 @@ const product = require('./modules/product');
 const cart = require('./modules/cart');
 const order = require('./modules/order');
 const others = require('./modules/others');
+const { default: mongoose } = require('mongoose');
 
 app.use('/', user)
 app.use('/', product)
---
+++ models/database.js
@@ -0,0 +1,13 @@
+//Testing connection - File might get removed for not in use :))
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    console.log('Connected')
+});
+
+//Models
+require('./category');
