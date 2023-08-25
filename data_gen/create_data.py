49620c0630a313eacd8236282b97a377eda562ff
vietanh00 <s3928859@rmit.edu.vn>
2023-08-25 16:36:54 +0700

    Create constraints; update category; improve product names

--- data_gen/create_data.py
+++ data_gen/create_data.py
@@ -4,50 +4,15 @@ import random
 
 FAKER = Faker()
 DATEFORMAT = "%Y-%m-%d %H:%M:%S"
-CATEGORY = {
-    "Electronics": {
-        "id": 1,
-        "parent": "none",
-        "attribute": "warranty",
-        "value": "6 months",
-        "required": "True"
-    },
-    "Phone and Accessories": {
-        "id": 2,
-        "parent": 1,
-        "attribute": "return",
-        "value": "1 week",
-        "required": "True"
-    },
-    "Laptop and Computer": {
-        "id": 3,
-        "parent": 1,
-        "attribute": "return",
-        "value": "1 month",
-        "required": "True"
-    },
-    "Clothing": {
-        "id": 4,
-        "parent": "none",
-        "attribute": "return",
-        "value": "1 week",
-        "required": "True"
-    },
-    "Men's Clothing": {
-        "id": 5,
-        "parent": 4,
-        "attribute": "discount on next purchase",
-        "value": "10%",
-        "required": "False"
-    },
-    "Women's Clothing": {
-        "id": 6,
-        "parent": 4,
-        "attribute": "discount on next purchase",
-        "value": "15%",
-        "required": "True"
-    },
-}
+
+# The categories below are the lowest levels. Products can only be of these categories.
+# For complete list of categories (i.e. these + parents), check mongoDB.
+CATEGORY = ["Phone", "Phone Accessories", "Computer", "Computer Accessories", 
+          "Refridgerator", "Cooker and Oven", "TV", "Lamps",
+          "Chairs and Sofas", "Bed", "Wall Decoration", "Floor Decoration",
+          "Male Clothing", "Male Accessories", "Boy Clothing and Accessories", "Female Clothing", "Female Accessories", "Girl Clothing and Accessories",
+          "Male Sportswear", "Female Sportswear", "Racket Sports", "Ball Sports"]
+
 
 # Export file names and header
 EXPORT = {
@@ -73,7 +38,7 @@ EXPORT = {
     },
     "order_details":{
         "fn": "./py_data/order_details.csv",
-        "header": ["id","customer_id","status","total_price"]
+        "header": ["order_id","customer_id","status","total_price"]
     },
     "order_item":{
         "fn": "./py_data/order_item.csv",
@@ -81,25 +46,24 @@ EXPORT = {
     }
 }
 
-#TODO: find out the highest id of each data file; set start_from values to the highest value +1
-# this should be done automatically
-
 def user(num_rows, start_from = 1):
     with open(EXPORT["user"]["fn"], 'w', newline='') as csvfile:
         csvw = csv.writer(csvfile)
         csvw.writerow(EXPORT["user"]["header"])
         for new_id in range(start_from, num_rows+1):
             uid = new_id
-            role = ['customer', 'seller', 'warehouse admin'][random.randint(0,2)]
-            user_name = FAKER.unique.word().capitalize() + FAKER.unique.word().capitalize() + str(random.randint(0,1000))
-            if role == 'seller':
+            role = ['Customer', 'Seller', 'Warehouse Admin'][random.randint(0,2)]
+            user_name = FAKER.simple_profile()['username']
+            display_name = ""
+            details = ""
+            if role == 'Seller':
                 display_name = FAKER.unique.company()
                 details = FAKER.paragraph() + " Created on: " + FAKER.date() + "." # description on the seller
-            elif role == 'customer':
+            elif role == 'Customer':
                 display_name = FAKER.unique.name()
                 details = FAKER.address().replace("\n", ", ") # customer address
-            elif role == 'warehouse admin':
-                display_name = FAKER.unique.name()
+            elif role == 'Warehouse Admin':
+                display_name = FAKER.unique.name() # warehouse admin's name
                 details = random.randint(1,50) # warehouse id
             password_hash = FAKER.md5()
             # write to csv
@@ -108,6 +72,7 @@ def user(num_rows, start_from = 1):
             # print(f"{uid} - {user_name} - {display_name}: {role} \n\t Details: {details} - Password hash: {password_hash}")
     return
 
+# Unused since we have a functional mongoDB now
 def category():
     with open(EXPORT["category"]["fn"], 'w', newline='') as csvfile:
         csvw = csv.writer(csvfile)
@@ -127,22 +92,43 @@ def category():
         # print(toString)
     return
 
+# help get descriptive name for product title
+def product_first_part(seed):
+    # use if else to only generate 1
+    if seed == 0:
+        return FAKER.color_name()
+    elif seed == 1:
+        return FAKER.company() + "'s"
+    elif seed == 2:
+        return FAKER.prefix() + " " + FAKER.last_name() + "'s"
+    elif seed == 3:
+        return ["Made", "Born"][random.randint(0,1)] + " in " + FAKER.country() + ":"
+    return ""
+
 def product(num_rows,start_from = 1):
+    sellers = [5,8,11,15,16,23,26,28,33,34,35,38,41,50,51,52,
+               55,57,60,62,64,66,67,71,73,74,75,82,84,89,94,95,99,100]
     with open(EXPORT["product"]["fn"], 'w', newline='') as csvfile:
         csvw = csv.writer(csvfile)
         csvw.writerow(EXPORT["product"]["header"])
+        first_adj = ["color", "company'S", "prefix + name's", "Made/Born in Country"]
+        last_adj = ["Edition ", "Gen. ", "MK ", "Version ", "No. ", "Variance ", "Prototype #"]
         for new_id in range(start_from, num_rows+1):
             pid = new_id
-            title = FAKER.word(part_of_speech='adjective').capitalize() + " " + FAKER.word(part_of_speech='noun').capitalize() + " MK" + str(random.randint(1,1000))
-            seller_id = random.randint(1, 100)
+            title = (f"{product_first_part(random.randint(0,3))} " # first adj
+                     f"{FAKER.word(part_of_speech='adjective').capitalize()} {FAKER.word(part_of_speech='noun').capitalize()} "
+                     f"{last_adj[random.randint(0,6)]}{str(random.randint(1,1000))}") # last adj
+            seller_id = sellers[random.randint(0, len(sellers) - 1)]
             price = random.randint(1000, 1000000)
-            category = random.randint(1,6) # category id instead of name
+            category = CATEGORY[random.randint(0, len(CATEGORY) - 1)] # category name instead of id
             length = random.randint(10,150)
             width = random.randint(10,90)
             height = random.randint(10,150)
             image = FAKER.file_name(category='image')
-            created_at = FAKER.date_time().strftime(DATEFORMAT) + ".000000"
-            updated_at = FAKER.date_time().strftime(DATEFORMAT) + ".000000"
+            date1 = FAKER.date_time()
+            date2 = FAKER.date_time()
+            created_at = (min(date1, date2)).strftime(DATEFORMAT) + ".000000"
+            updated_at = (max(date1, date2)).strftime(DATEFORMAT) + ".000000"
             # write to csv
             csvw.writerow([pid, title, seller_id, price, category, length, width, height, image, created_at, updated_at])
             
@@ -174,16 +160,25 @@ def warehouse(num_rows, start_from = 1):
         
     return
 
-def cart_details(num_rows, start_from = 1):
+USER = [6,9,10,12,13,18,19,27,30,32,36,37,42,44,45,48,49,54,56,59,63,65,68,69,79,80,83,85,90,92,93,96,97,98]
+def cart_details(start_from = 1):
     with open(EXPORT["cart_details"]["fn"], 'w', newline='') as csvfile:
         csvw = csv.writer(csvfile)
         csvw.writerow(EXPORT["cart_details"]["header"])
-        for row_num in range (start_from, num_rows+1):
-            customer_id = row_num
-            product_id = random.randint(1,100)
-            quantity = random.randint(1,20)
-            # write to csv
-            csvw.writerow([customer_id, product_id, quantity])
+        for i in range (start_from - 1, len(USER)):
+            customer_id = USER[i]
+            # this is a very elementary way to implement weight
+            existing = [] # make sure that (customer_id, pid) is unique
+            weight = [0,1,1,1,1,1,2,2,2,2,2,3,3,3]
+            for i in range(0, weight[random.randint(0, len(weight) - 1)]):
+                pid = random.randint(1,200)
+                while pid in existing:
+                    pid = random.randint(1,100)
+                existing.append(pid)
+                
+                quantity = random.randint(1,20)
+                # write to csv
+                csvw.writerow([customer_id, pid, quantity])
             
             # print to debug
             # toString = f"Row {row_num}: Customer {customer_id}'s cart has {quantity} of item {product_id}"
@@ -196,7 +191,7 @@ def order_details(num_rows, start_from = 1):
         csvw.writerow(EXPORT["order_details"]["header"])
         for new_id in range (start_from, num_rows+1):
             order_id = new_id
-            customer_id = random.randint(1,100)
+            customer_id = USER[random.randint(0,len(USER) - 1)]
             status = ['Accepted', 'Inbound', 'Rejected'][random.randint(0,2)]
             total_price = random.randint(10000,1000000)
             # write to csv
@@ -207,17 +202,23 @@ def order_details(num_rows, start_from = 1):
             # print(toString)
     return
 
-#TODO: make sure (order_id, product_id) is unique
-def order_item(num_rows, start_from = 1):
+def order_item(num_users, start_from = 1):
     with open(EXPORT["order_item"]["fn"], 'w', newline='') as csvfile:
         csvw = csv.writer(csvfile)
         csvw.writerow(EXPORT["order_item"]["header"])
-        for row_num in range (start_from, num_rows+1):
-            order_id = random.randint(1,100)
-            product_id = random.randint(1,100)
-            quantity = random.randint(1,20)
-            # write to csv
-            csvw.writerow([order_id, product_id, quantity])
+        for oid in range (start_from, num_users + 1):
+            order_id = oid
+            weight = [1,1,1,1,1,1,2,2,2,2,2,3]
+            existing = [] # make sure that (oid, pid) is unique
+            for i in range(0, weight[random.randint(0, len(weight) - 1)]):
+                pid = random.randint(1,100)
+                while pid in existing:
+                    pid = random.randint(1,100)
+                existing.append(pid)
+                
+                quantity = random.randint(1,20)
+                # write to csv
+                csvw.writerow([order_id, pid, quantity])
             
             # print to debug
             # toString = f"Row {row_num}: Order {order_id} has {quantity} of item {product_id}"
@@ -225,13 +226,13 @@ def order_item(num_rows, start_from = 1):
     return
 
 def main():
-    user(50)
-    product(150)
-    category()
-    warehouse(20)
-    cart_details(100)
-    order_details(100)
-    order_item(100)
+    # user(100)
+    # product(200)
+    # category()
+    # warehouse(20)
+    cart_details()
+    # order_details(100)
+    # order_item(100)
     return 0
 
 main()
