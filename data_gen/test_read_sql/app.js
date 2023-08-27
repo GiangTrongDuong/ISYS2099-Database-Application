27ea56f04377cf7528e53fa9e1a187fa5f5df448
vietanh00 <s3928859@rmit.edu.vn>
2023-08-27 16:59:20 +0700

    example reading mysql

---
+++ data_gen/test_read_sql/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const dataRead = require('./read_data');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use('/read', dataRead);
+
+// full route to Home page: /
+app.get("/", function (req, res) {
+  res.render('home/index')
+});
+
+app.listen(port, function () {
+  console.log("Server started on port 3000");
+});
+
+app.use('./database', dataRead);
---
+++ data_gen/test_read_sql/database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config();
+
+
+const connection = mysql.createConnection({
+    multipleStatements: true,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+connection.connect(err => {
+    if (err) {
+        fs.appendFile('error.log', `===== Start connection: ${err}\n`, (fileErr) => {
+            if (fileErr) {
+                console.error('Error writing to error.log:', fileErr);
+            }
+        });
+        return;
+    }
+    console.log('Connected to the database');
+});
+module.exports = connection;
+
+
