297eb0115fa627ae1fa25d82dde7e8439ec88b15
vietanh00 <s3928859@rmit.edu.vn>
2023-08-31 16:13:43 +0700

    Change product.category to mongo _id

---
+++ data_gen/sql_data/replace_category.py
@@ -0,0 +1,25 @@
+import json
+
+catfile = '../cat_att_list.json'
+nf = './prod.txt'
+out = './newprod.txt'
+
+# record object['_id']['$oid'] and object['name']
+f = open(catfile)
+cats = json.load(f)
+record_cat = {}
+for cat in cats:
+    record_cat[cat["name"]] = cat["_id"]["$oid"]
+f.close()
+# open db_inserts.sql; replace name with id
+input_file  = open(nf, 'r')
+output_file = open(out, 'w')
+for count,line in enumerate(input_file):
+    print("====== Line " + str(count))
+    for cat_name in list(record_cat.keys()):
+        wrapped_cat = f"\'{cat_name}\'"
+        if(wrapped_cat in line):
+            print(wrapped_cat)
+            modified_line = line.replace(wrapped_cat, f"\'{record_cat[cat_name]}\'")
+            output_file.write(modified_line)
+            break
