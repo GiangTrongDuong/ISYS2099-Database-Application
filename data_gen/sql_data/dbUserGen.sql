0d12c8ab9ac58be6bb8967bf0f215b54ce377181
Giang Trong Duong <trongduong04112003@gmail.com>
2023-09-04 16:52:49 +0700

    Generate user for db

---
+++ data_gen/sql_data/dbUserGen.sql
@@ -0,0 +1,47 @@
+-- Guest priviledge (read only, write for user table)
+CREATE USER 'guest'@'localhost'
+IDENTIFIED BY 'guestpass';
+
+GRANT SELECT
+ON testg2.product
+TO 'guest'@'localhost';
+
+GRANT SELECT,INSERT
+ON testg2.user
+TO 'guest'@'localhost';
+
+-- User Priviledge --
+CREATE USER 'user'@'localhost'
+IDENTIFIED BY 'userpass';
+
+GRANT SELECT 
+ON testg2.product
+TO 'user'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE
+ON testg2.user
+TO 'user'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE
+ON testg2.user
+TO 'user'@'localhost';
+
+-- Seller
+CREATE USER 'seller'@'localhost'
+IDENTIFIED BY 'sellerpass';
+
+GRANT SELECT, INSERT, UPDATE
+ON testg2.product
+TO 'seller'@'localhost';
+
+GRANT INSERT, UPDATE
+ON testg2.warehouse
+TO 'seller'@'localhost';
+
+-- Warehouse
+CREATE USER 'warehouse'@'localhost'
+IDENTIFIED BY 'warepass';
+
+GRANT DELETE
+ON testg2.warehouse
+TO 'warehouse'@'localhost';
