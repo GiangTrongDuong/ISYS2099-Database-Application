74003d3641676b7cb471c4ec8b94e0e6222b0250
hakhanhne <s3877707@rmit.edu.vn>
2023-09-06 01:23:20 +0700

    getAllCats, deleteCat, deleteCatAndChildren, route

--- data_gen/test_mongo/test_mongo.js
+++ data_gen/test_mongo/test_mongo.js
@@ -20,12 +20,13 @@ app.get("/category/get-all-children/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const result = await mg.getAllChildren(id);
-    sendResponse(res, 200, `ok`, result);
+    if (result) sendResponse(res, 200, `ok`, result);
+    else sendResponse(res, 404, `No category with id ${id} is found`, null);
+
   } catch (err) {
     console.log(err)
     sendResponse(res, 500, `Error ${err}`);
   }
-
 });
 
 app.post("/category", async (req, res) => {
@@ -33,6 +34,41 @@ app.post("/category", async (req, res) => {
     const {_id, name, attribute, parent_category} = req.body;
     const result = await mg.saveCat(_id, name, attribute, parent_category);
     sendResponse(res, 200, `ok`, result);
+  } catch (err) {
+    console.log(err)
+    sendResponse(res, 500, `Error ${err}`);
+  }
+});
+
+app.get("/category", async (req, res) => {
+  try {
+    const result = await mg.getAllCats();
+    sendResponse(res, 200, `ok`, result);
+  } catch (err) {
+    console.log(err)
+    sendResponse(res, 500, `Error ${err}`);
+  }
+});
+
+app.delete("/category/delete-cat-only/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await mg.deleteCat(id);
+    if (result) sendResponse(res, 200, `Transaction succeeded. Category is deleted.`);
+    else sendResponse(res, 500, `Transaction failed. Category is not deleted.`);
+  } catch (err) {
+    console.log(err)
+    sendResponse(res, 500, `Error ${err}`);
+  }
+});
+
+app.delete("/category/delete-cat-and-children/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await mg.deleteCatAndChildren(id);
+    if (result.length != 0) sendResponse(res, 200, `Category and its children are deleted.`, result);
+    if (result.length == 0) sendResponse(res, 404, `No category with ID ${id} is found.`, result);
+    else sendResponse(res, 500, `Transaction failed. Category and its children are not deleted.`);
   } catch (err) {
     console.log(err)
     sendResponse(res, 500, `Error ${err}`);
