af751e429729b53c9b1322fe8f83f069824cf084
vietanh00 <s3928859@rmit.edu.vn>
2023-09-06 17:19:43 +0700

    Done function for placing orders

--- models/function_order.js
+++ models/function_order.js
@@ -31,39 +31,80 @@ async function get_order_item (oid) {
 
 // pass in param: [{'pid': 2, 'quantity': 10}, {...}]
 async function place_order(uid, product_quantity_list){
-    try{
-        //Update this line later with the total price!
-            //Do not add order_id in - it's auto increment!
-        database.query(`INSERT INTO order_details (customer_id, status,total_price, created_at)
-            VALUES (${uid}, \'Inbound\', 0, \'${getCurrentTimeString()}\');`, async (err, result) => {
-                if (err) return console.log("Insert error: " + err);
-                const newOrderId = result.insertId;
-                let total_price = 0;
-                for (pair of product_quantity_list){
-                    const {pid, quantity} = pair;
-                    database.query(
-                        `INSERT INTO order_item (order_id, product_id, quantity) 
-                        VALUES (${newOrderId}, ${pid}, ${quantity});`, async(err1, result) =>{
-                            if (err1) console.log("Insert order item error: " + err1);
-                        });
-                    var product_price = await getPrice(pid);
-                    // console.log("\t MMM "+ product_price);
-                    total_price += product_price * parseInt(quantity);
-                }
-                database.query(
-                    `UPDATE order_details 
-                    SET total_price = ${total_price}
-                    WHERE id = ${newOrderId};`, function(err2, result){
-                        if (err2) return console.log("Error update after insert: " + err2);
+    return new Promise((resolve, reject) => {
+        try{
+            //Update this line later with the total price!
+                //Do not add order_id in - it's auto increment!
+            database.query(`INSERT INTO order_details (customer_id, status,total_price, created_at)
+                VALUES (${uid}, \'Inbound\', 0, \'${getCurrentTimeString()}\');`, async (err, result) => {
+                    if (err) {
+                        console.log("Insert order_details error" + err)
+                        reject ({"Insert order_details error" : err});
+                    }
+                    const newOrderId = result.insertId;
+                    console.log("New order created: " + newOrderId);
+                    for (pair of product_quantity_list){
+                        const {pid, quantity} = pair;
+                        // CALL (order_trans(oid, pid, quant, OUT cost))
+                        database.query(
+                            `CALL order_trans(${newOrderId},${pid}, ${quantity}, @cost);`, function(err1, result1) {
+                                if (err1) {
+                                    console.log("Place item error" + err1);
+                                    // if 1 item fails, the rest should continue
+                                    // reject({"Insert order item error" : err1});
+                                }
+                                console.log("Item inserted:" + result1)
+                            });
+                    }
+    
+                    // Get the order's total price in order_item. If price = 0, delete this order.
+                    let total_price = 0;
+                    database.query(`SELECT SUM(p.price * o.quantity) as order_cost
+                    FROM product p JOIN order_item o ON p.id = o.product_id WHERE o.order_id = ${newOrderId}
+                    GROUP BY o.order_id;`, function(err2, result2) {
+                        try{
+                            total_price = result2[0].order_cost;
+                            console.log("=========== Total price " + total_price);
+                            database.query(
+                                `UPDATE order_details 
+                                SET total_price = ${total_price}
+                                WHERE id = ${newOrderId};`, function(err4, result){
+                                    if (err4) {
+                                        console.log("Update after insert "+ err4);
+                                    }
+                                    console.log("Order placed. oid: " + newOrderId);
+                                    resolve ({"message": `Order placed successful! Go to localhost:3000/order/${newOrderId} to check!`});
+                                });
+                        }
+                        catch (err25){
+                            console.log("Not enough item in stock.");
+                            database.query(`DELETE FROM order_details WHERE id = ${newOrderId};`, function(err3, result3){
+                                if (err3) {
+                                    console.log("Delete error" + err3);
+                                    reject({"Delete error":err3});
+                                }
+                                console.log("No order was created, because the items you placed went out of stock.");
+                                resolve({"message":"No order was created, because the items you placed went out of stock."});
+                            });
+                        }
                     });
-                // TODO: Get this one to show to see new order id
-                return ({"message": `Order placed successful! Go to localhost:3000/order/${newOrderId} to check!`});
-            });
-        console.log("Order details and item inserted.");
-        return({"message":"Your order has been placed!"});
+                });
+        }
+        catch (err){
+            reject({"message": "Error placing order: " + err});
+        }
+    });
+}
+
+async function simulate_orders(){
+    try{
+        var o1 = place_order(6, [{"pid":2, "quantity":2}]);
+        var o2 = place_order(9, [{"pid":2, "quantity":2}]);
+        Promise.all([o1, o2])
+          .then(results => console.log(results)); // [promise1Result, promise2Result]
     }
     catch (err){
-        return({"message": "Error placing order: " + err});
+        console.log(err);
     }
 }
 
